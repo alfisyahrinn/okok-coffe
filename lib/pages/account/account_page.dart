e1060ffa03c1b62b1ff016f77fead15b7f0d5771
Alfi Syahrin <sahrin201@gmail.com>
2023-12-14 17:30:32 +0700

    feat(account) silicing ui account picture, name, email

--- lib/pages/account/account_page.dart
+++ lib/pages/account/account_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:okok_coffe/utils/color.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -12,13 +12,52 @@ class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Account"),
-      ),
-      body: Center(
-        child: Text("Account Page"),
-      ),
-      // bottomNavigationBar: Navbar(),
-    );
+        appBar: AppBar(
+          title: Center(
+            child: Text("Account"),
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage:
+                        NetworkImage('https://via.placeholder.com/150'),
+                  ),
+                  SizedBox(
+                      width:
+                          20), // Menambahkan jarak antara CircleAvatar dan teks
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Nama Pengguna',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: MyColor.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'email@example.com',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
