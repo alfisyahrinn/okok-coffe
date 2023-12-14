b4ffdeed4c5cc16b4a8c0c9b5d19af358bf832f3
Alfi Syahrin <sahrin201@gmail.com>
2023-12-14 17:29:44 +0700

    chore(keranjang) add total harga dan chekout tombol

--- lib/pages/keranjang/keranjang_page.dart
+++ lib/pages/keranjang/keranjang_page.dart
@@ -12,80 +12,158 @@ class _KeranjangPageState extends State<KeranjangPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: Center(
-            child: Text(
-              "Keranjang",
-              style: TextStyle(
-                color: MyColor.primary,
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Center(
+          child: Text(
+            "Keranjang",
+            style: TextStyle(
+              color: MyColor.primary,
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
             ),
           ),
         ),
-        body: ListView.builder(
-          itemCount: 15,
-          itemBuilder: (context, index) {
-            return Card(
-              color: Colors.white70,
-              margin: const EdgeInsets.all(8),
-              child: ListTile(
-                leading: Container(
-                  height: 58,
-                  width: 58,
-                  alignment: Alignment.center,
-                  decoration: ShapeDecoration(
-                    color: MyColor.primary,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)),
+      ),
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: 15,
+            itemBuilder: (context, index) {
+              return Card(
+                color: Colors.white70,
+                margin: const EdgeInsets.all(8),
+                child: ListTile(
+                  leading: Container(
+                    height: 58,
+                    width: 58,
+                    alignment: Alignment.center,
+                    decoration: ShapeDecoration(
+                      color: MyColor.primary,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                    child: Text(
+                      "01",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 31,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
                   ),
-                  child: Text(
-                    "01",
+                  title: Text(
+                    "Udin",
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 31,
+                      // fontSize: 14,
+                      color: MyColor.primary,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "Rp.200.000",
+                    style: TextStyle(
+                      // fontSize: 10,
+                      color: MyColor.primary,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                ),
-                title: Text(
-                  "Udin",
-                  style: TextStyle(
-                    // fontSize: 14,
-                    color: MyColor.primary,
-                    fontWeight: FontWeight.w600,
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      final snackBar = SnackBar(
+                        content: Text('${index} Berhasil di Hapus'),
+                        backgroundColor: (Colors.black12),
+                        action: SnackBarAction(
+                          label: 'hide',
+                          onPressed: () {},
+                        ),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
                   ),
                 ),
-                subtitle: Text(
-                  "Rp.200.000",
-                  style: TextStyle(
-                    // fontSize: 10,
-                    color: MyColor.primary,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    final snackBar = SnackBar(
-                      content: Text('${index} Berhasil di Hapus'),
-                      backgroundColor: (Colors.black12),
-                      action: SnackBarAction(
-                        label: 'hide',
-                        onPressed: () {},
-                      ),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
+              );
+            },
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Colors.black38,
+                    Colors.black45,
+                    Colors.black54,
+                    Colors.transparent,
+                  ],
                 ),
               ),
-            );
-          },
-        )
-        // bottomNavigationBar: Navbar(),
-        );
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Total Harga : ', // Ganti dengan nilai total harga Anda
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          'Rp. 204.000', // Ganti dengan nilai total harga Anda
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Action when the "Checkout" button is pressed
+                      },
+                      child: Text(
+                        'Checkout',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: MyColor.primary,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      // bottomNavigationBar: Navbar(),
+    );
   }
 }
