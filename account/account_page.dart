6c441962440223532a84194be2bd2b84ce369e1b
Mitha Zahara <112945819+mithazahara@users.noreply.github.com>
2023-12-18 21:48:29 +0700

    front-end account

---
+++ account/account_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class AccountPage extends StatefulWidget {
+  const AccountPage({super.key});
+
+  @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Account"),
+      ),
+      body: Center(
+        child: Text("Account Page"),
+      ),
+      // bottomNavigationBar: Navbar(),
+    );
+  }
+}
