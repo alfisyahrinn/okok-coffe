eb3cd2e25ff384f38e6566354fb8d92a9ee95807
Alfi Syahrin <sahrin201@gmail.com>
2023-12-24 18:49:58 +0700

    chore(keranjang) fix qty not update if push to transaksi

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
+  @override                                                                                                             
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
